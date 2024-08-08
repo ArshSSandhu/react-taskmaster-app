cd9d4cde22d7e57461cce7d938d126515e306703
Arshpreet Singh Sandhu <sanda09@pfw.edu>
2024-08-08 17:19:44 -0400

    fixed import

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 
 import './App.css';
 import Header from './MyComponents/Header';
-import {Todos} from './MyComponents/Todos';
-import {TodoItem} from './MyComponents/TodoItem';
-import {Footer} from './MyComponents/Footer'
+import Todos from './MyComponents/Todos';
+import TodoItem from './MyComponents/TodoItem';
+import Footer from './MyComponents/Footer'
 
 function App() {
   return (
