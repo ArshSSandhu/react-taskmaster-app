d677e8390e1e314edab73586a830025cf3cffb4b
Arshpreet Singh Sandhu <sanda09@pfw.edu>
2024-08-09 16:13:37 -0400

    implemented delete logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 
+import { useState } from 'react';
 
 import './App.css';
 import Header from './MyComponents/Header';
@@ -6,8 +7,19 @@ import Todos from './MyComponents/Todos';
 import Footer from './MyComponents/Footer'
 
 function App() {
+
+  const onDelete = (todo)=>{
+    console.log("I am onDelete", todo)
+  
+//if this is not the item I want to delete, keep in a new array
+setTodos(todos.filter((e)=>{
+  return e!==todo;
+}))
+
+  }
+
 // object
-let todos = [
+const [todos, setTodos] = useState([
   {
     sno: 1,
     title: "Go Gym",
@@ -23,13 +35,13 @@ let todos = [
     title: "Go lib",
     desc: "study react"
   }
-]
+]);
 
   return (
     <>
     <Header title="My TaskMaster App" searchBar={false} />
 
-    <Todos todos = {todos}/>
+    <Todos todos = {todos} onDelete={onDelete} />
     <Footer/>
     </>
   );
